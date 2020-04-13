963fe0efc5e371e1aa9fe9212aec5e77f465cc68
Joe Houng <sjhoung@gmail.com>
2020-04-12 19:11:27 -0700

    Use azure speech for speech recognition and word offsets

---
+++ autex/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace autex
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.CognitiveServices.Speech;
+    using Microsoft.CognitiveServices.Speech.Audio;
+    class Program
+    {
+        static async Task Main()
+        {
+            await RecognizeSpeechAsync();
+        }
+
+        static async Task RecognizeSpeechAsync()
+        {
+            var config = SpeechConfig.FromSubscription("940929eb2cfe4158a6e1229959726d4e", "eastus");
+            config.RequestWordLevelTimestamps();
+
+            using (var audioInput = AudioConfig.FromWavFileInput("C:/Users/sjhou/OneDrive/Documents/whosonfirst.wav"))
+            using (var recognizer = new SpeechRecognizer(config, audioInput))
+            {
+                Console.WriteLine("Recognizing first result...");
+                var result = await recognizer.RecognizeOnceAsync();
+
+                switch (result.Reason)
+                {
+                    case ResultReason.RecognizedSpeech:
+                        Console.WriteLine($"We recognized: {result.ToString()}");
+                        break;
+                    case ResultReason.NoMatch:
+                        Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                        break;
+                    case ResultReason.Canceled:
+                        var cancellation = CancellationDetails.FromResult(result);
+                        Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                        if (cancellation.Reason == CancellationReason.Error)
+                        {
+                            Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                            Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                            Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                        }
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
